b686ac884e10a5ce501a2159ddb0a2b863edfbf6
Macintosh <admin@Macintoshs-MacBook-Air.local>
2025-12-26 08:43:15 +0800

    initial commit

---
+++ footer.js
@@ -0,0 +1,8 @@
+// 公用 footer - 包含 Vercel 统计
+(function() {
+    // 添加 Vercel Analytics
+    const script = document.createElement('script');
+    script.defer = true;
+    script.src = '/_vercel/insights/script.js';
+    document.body.appendChild(script);
+})();
